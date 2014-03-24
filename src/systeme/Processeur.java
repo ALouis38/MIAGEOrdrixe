04533e151bab8c99ee266b42ce5ad491346f6514
louis <louis.a.ujf@gmail.com>
2014-03-24 17:30:12 +0100

    Processeur (incomplet)

--- src/systeme/Processeur.java
+++ src/systeme/Processeur.java
@@ -1,5 +1,25 @@
 package systeme;
 
+import instructions.Instruction;
+
 public class Processeur {
 
+	Memoire memoire;
+	Instruction registreInstruction;
+	int compteurOrdinal;
+		
+	public Processeur(Memoire memoire){
+		this.memoire = memoire;
+		compteurOrdinal = 0;
+		registreInstruction = memoire.instruction(compteurOrdinal);
+	}
+	
+	public void execute(){
+		registreInstruction.execute();
+	}
+	
+	public void chargerInstruction(){
+		compteurOrdinal++;
+		registreInstruction = memoire.instruction(compteurOrdinal);
+	}
 }
