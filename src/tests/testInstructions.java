f97be7b37975f752f5890ef06520b90202cde504
cutroneg <guillaume.cutrone@gmail.com>
2014-03-24 17:22:44 +0100

    tests instructions OK

--- src/tests/testInstructions.java
+++ src/tests/testInstructions.java
@@ -1,10 +1,34 @@
 package tests;
 
+import instructions.*;
+
 public class testInstructions {
 
 	public static void main(String[] args) {
 		
+		// instanciation des opérandes
+		Operande op1 = new Operande(" ", 4);
+		System.out.println(op1.valeur());
+		Operande op2 = new Operande(" ", 2);
+		System.out.println(op2.valeur());
+		
+		// instanciation des inst 
+		Add addInst = new Add(op1, op2);
+		Sub subInst = new Sub(op1, op2);
+		Mul mulInst = new Mul(op1, op2);
+		Div divInst = new Div(op1, op2);
+		Mod modInst = new Mod(op1, op2);
+		
+		// test des execute (n'en executer qu'un seul à la fois)
+		
+		addInst.execute();
+//		subInst.execute();
+//		mulInst.execute();
+//		divInst.execute();
+//		modInst.execute();
 
+		// debug du resultat
+		System.out.println(op2.valeur());
 	}
 
 }
