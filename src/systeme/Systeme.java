8f8b72da80adede5d288a3ba72d53ee4741717db
louis <louis.a.ujf@gmail.com>
2014-04-03 13:22:30 +0200

    système : boucle d'exécution

--- src/systeme/Systeme.java
+++ src/systeme/Systeme.java
@@ -8,8 +8,22 @@ public class Systeme {
 	Memoire memoire;
 	Processeur processeur;
 	Processus [] listeProcessus;
+	int nbProcessusVivants;
 	
-	public static void main(String[] args) {
+	public Systeme(){
+		nbProcessusVivants = listeProcessus.length;
+		int i = 0;
 		
+		while(nbProcessusVivants > 0){
+			i = i % listeProcessus.length;
+				if (listeProcessus[i].vivant()) {
+					processeur.execute(listeProcessus[i].adresseCourrante());
+					if (listeProcessus[i].adresseCourrante() == -1) {
+						listeProcessus[i].mourir();
+						nbProcessusVivants--;
+					}
+				}
+			i++;
+		}
 	}
 }
