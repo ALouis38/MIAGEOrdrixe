6f14ffa92ca8626ebda8625fd13ec5b2daa4b1e7
cutroneg <guillaume.cutrone@gmail.com>
2014-04-04 18:10:33 +0200

    vue

--- src/vue/VuePrincipale.java
+++ src/vue/VuePrincipale.java
@@ -1,5 +1,6 @@
 package vue;
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,51 +8,35 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class VuePrincipale extends JFrame {
-	
+	private JTable table;
+
 	public static void main(String[] args) throws Exception {
 		new VuePrincipale();
 	}
-	
-	public VuePrincipale(){
+
+	public VuePrincipale() {
+		getContentPane().setLayout(null);
+
+
+
+		String data[][] = { { "Vinod", "5000", "Java" },
+				{ "Deepak", "14000", "developer" },
+				{ "Sushil", "20000", "writter" },
+				{ "Chandan", "15000", "Programmer" } };
+		String col[] = { "Emp_name", "Emp_sal", "Depart" };
+		
+		table = new JTable(data,col);
+		table.setBounds(105, 112, 1, 1);
+
+		table.setRowHeight(40);
+		table.setRowMargin(4);
+
+		getContentPane().add(table);
 		this.initialize();
 	}
-	
-	
-	
-	public void initialize(){
-		
-		//Main Frame
-		setResizable(false);
-		setTitle("Ordrixe");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setMinimumSize(new Dimension(1375, 582));
-		setLocationRelativeTo(null);
-		setVisible(true);
-		
-		//Menu		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
-		JMenu mnFichier = new JMenu("Fichier");
-		menuBar.add(mnFichier);
-		
-		JMenuItem mntmNouveauJeu = new JMenuItem("Nouveau jeu");
-		mntmNouveauJeu.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-			}
-		});
-		mnFichier.add(mntmNouveauJeu);
-		JSeparator separator = new JSeparator();
-		mnFichier.add(separator);
-		JMenuItem mntmQuitter = new JMenuItem("Quitter");
-		mntmQuitter.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				if (JOptionPane.showConfirmDialog(null, "Souhaitez-vous quitter l'application?", "Quitter", JOptionPane.OK_CANCEL_OPTION) == 0) dispose();
-			}
-		});
-		mnFichier.add(mntmQuitter);
-		
-		
+
+	public void initialize() {
+		this.setVisible(true);
+		//http://www.roseindia.net/java/example/java/swing/GetRowsAndCols.shtml
 	}
 }
